cf7946b00f1586eaaecf531eabe27ee7b8dcb9df
shiwangupadhyay <ushiwang1244a@gmail.com>
2025-02-01 09:56:01 +0530

    project structure ready venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Shiwang Upadhyay",
+    author_email="ushiwang1244a@gmail.com",
+    packages=find_packages()
+)
